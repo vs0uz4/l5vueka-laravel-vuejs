4b99f205c03df9770e53ef6f36247fa35236c1ee
Vitor S Rodrigues <vitor.rodrigues@gmail.com>
2017-09-29 06:31:19 -0300

    Install Browsersync dependency and Refactore gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,38 @@ elixir((mix) => {
      */
     mix.sass('app.scss');
 
+    /**
+     *  Concatenating font-awesome styles files in vendor.css
+     */
     mix.styles([
         './node_modules/font-awesome/css/font-awesome.css'
     ], 'public/css/vendor.css');
 
-    mix.copy('node_modules/font-awesome/fonts', 'public/fonts');
+    /**
+     *  Coping fonts of font-awesome for public/build/fonts path
+     */
+    mix.copy('node_modules/font-awesome/fonts', 'public/build/fonts');
 
     /**
      *  Building Javascript files
      */
     mix.webpack('app.js');
+
+    /**
+     *  Versioning files
+     */
+    mix.version([
+       'public/css/vendor.css',
+       'public/css/app.css',
+       'public/js/app.js',
+    ]);
+
+    /**
+     *  Syncing files in Browser
+     */
+    mix.browserSync({
+        proxy: 'app:8080',
+        open: false,
+    });
+
 });
