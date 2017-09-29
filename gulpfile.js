f083bbddc33afa1c3b1dc28a602aa25a87bd6af9
Vitor S Rodrigues <vitor.rodrigues@gmail.com>
2017-09-29 05:42:34 -0300

    Implemented font-awesome in project - changed gulpfile; - changed welcome view; - rebuilt assets with gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,12 @@ elixir((mix) => {
      */
     mix.sass('app.scss');
 
+    mix.styles([
+        './node_modules/font-awesome/css/font-awesome.css'
+    ], 'public/css/vendor.css');
+
+    mix.copy('node_modules/font-awesome/fonts', 'public/fonts');
+
     /**
      *  Building Javascript files
      */
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,11 +6,14 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bem Vindos so Curso!</title>
+    <link rel="stylesheet" href="{{ asset('css/vendor.css') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     <div class="container">
-        <h1>Curso Laravel e Vue.Js</h1>
+        <h1>
+            <i class="fa fa-fw fa-github"></i>Curso Laravel e Vue.Js
+        </h1>
     </div>
 
     <script src="{{ asset('js/app.js') }}"></script>
