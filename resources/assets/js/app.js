b8434ac26e5fac5f7258e4ce86bdc336f35b994e
Vitor S Rodrigues <vitor.rodrigues@gmail.com>
2017-10-03 06:37:28 -0300

    Refactored app.js for remove vue-resource and added axios.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,12 +4,12 @@ window.jQuery = window.$ = require('jquery')
 require('bootstrap-sass')
 
 import Vue from 'vue'
-import VueResource from 'vue-resource'
+import axios from 'axios'
 
 import PrimeiroComponente from './app/components/primeiro-componente.vue'
 import VdUsers from './app/users/main.vue'
 
-Vue.use(VueResource)
+Vue.prototype.$http = axios
 
 new Vue({
     el: '#app',
